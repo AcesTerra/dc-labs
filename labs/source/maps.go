8e13de412c9c3336fc68bb17994e2de92936071a
root <root@alfredoe-MOBL.amr.corp.intel.com>
2020-01-29 16:57:26 -0600

    Source for maps
    Signed-off-by: root <root@alfredoe-MOBL.amr.corp.intel.com>

---
+++ labs/source/maps.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	       "golang.org/x/tour/wc"
+	              "strings"
+	      )
+
+	      func WordCount(s string) map[string]int {
+
+		             var stringArray []string = strings.Fields(s)
+			            testMap := make(map[string]int)
+				           for _, word := range stringArray {
+						                 count, ok := testMap[word]
+								               if ok {
+										                            testMap[word] = count + 1
+													                  } else {
+																                       testMap[word] = 1
+																		                     }
+																				            }
+																					           return testMap
+																					   }
+
+																					   func main() {
+																						          wc.Test(WordCount)
+																						  }
