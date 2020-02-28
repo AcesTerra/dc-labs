16dae5a745a222c4c735cf7e4e7f048eae7b864f
Alfredo Emmanuel Garcia Falcon <jovengf@gmail.com>
2020-02-27 19:09:33 -0600

    N go routines created given a number
    Signed-off-by: Alfredo Emmanuel Garcia Falcon <jovengf@gmail.com>

--- labs/goroutines-vs-os-threads/excercise_1.go
+++ labs/goroutines-vs-os-threads/excercise_1.go
@@ -23,20 +23,20 @@ func goRoutineLoop(returnToMain chan int, nextGoRoutine chan int, limit int){
 func main(){
 	mainChannel := make(chan int)
 	goRoutineChannel := make(chan int)
-	limit := 100
-	go goRoutineLoop(mainChannel, goRoutineChannel, limit)
+	start := 1
+	var limit int
+	_, err := fmt.Scanf("%d", &limit)
+	if (err == nil){
+		//fmt.Println(v)
+		fmt.Println(time.Now().Format(time.RFC850))
+		go goRoutineLoop(mainChannel, goRoutineChannel, limit)
+		goRoutineChannel <- start
+	}
 	//reader := bufio.NewReader(os.Stdin)
 	//fmt.Print("Enter a number: ")
 	//text, _ := reader.ReadString('\n')
 	//fmt.Println(text)
-	var v int
-	_, err := fmt.Scanf("%d", &v)
-	if (err == nil){
-		//fmt.Println(v)
-		fmt.Println(time.Now().Format(time.RFC850))
-		goRoutineChannel <- v
-	}
-	r := <- mainChannel
+	<- mainChannel
 	fmt.Println(time.Now().Format(time.RFC850))
-	fmt.Println(r)
+	//fmt.Println(r)
 }
