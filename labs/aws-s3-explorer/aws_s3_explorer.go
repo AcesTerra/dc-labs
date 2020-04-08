9c5bc59ae3988c4ec7c73cba7c64ce5b7e1c94ce
Alfredo Emmanuel Garcia Falcon <jovengf@gmail.com>
2020-04-08 17:14:06 -0500

    Comments added
    Signed-off-by: Alfredo Emmanuel Garcia Falcon <jovengf@gmail.com>

--- labs/aws-s3-explorer/aws_s3_explorer.go
+++ labs/aws-s3-explorer/aws_s3_explorer.go
@@ -6,34 +6,33 @@ import(
 	"net/http"
 	"encoding/xml"
 	"log"
-	//"reflect"
 	"io/ioutil"
 	"bytes"
 	"strconv"
 	"strings"
 )
 
+//Structure to store the content of XML
 type ListBucketResult struct {
 	Content	[]Contents `xml:"Contents"`
 }
 
+//Structure to get info from XML contents
 type Contents struct {
 	Key	string `xml:"Key"`
 }
 
+//Function to get the extensions and count it
 func getContent(content []Contents) (int, string){
 	directoriesCntr := 0
 	noExtension := 0
 	extensionsMap := make(map[string]int)
 	for _, item := range content{
 		name := string(item.Key)
-		//fmt.Println(reflect.TypeOf(name))
 		if(name[len(name)-1] == '/'){
-			//fmt.Println(name)
 			directoriesCntr++
 		} else{
 			extension := strings.Split(name, ".")
-			//fmt.Println(extension)
 			if (len(extension) < 2){
 				noExtension++
 			} else{
@@ -47,17 +46,15 @@ func getContent(content []Contents) (int, string){
 		}
 	}
 	extensionsMap["no-extension"] = noExtension
-	//fmt.Println(extensionsMap)
 	extensions := ""
 	for key, value := range extensionsMap{
 		extensions = extensions + key + "(" + strconv.Itoa(value) + "), "
 	}
-	//fmt.Println(extensions)
 	extensions = extensions[0:len(extensions)-2]
-	//fmt.Println(extensions)
 	return directoriesCntr, extensions
 }
 
+//Function that get the http response and translate it to text
 func getXML(url string) (string){
 	resp, err := http.Get(url)
 	if err != nil {
@@ -68,13 +65,11 @@ func getXML(url string) (string){
 	if err != nil {
 		log.Fatal(err)
 	}
-	//fmt.Println(reflect.TypeOf(rawXML))
 	return string(rawXML)
 }
 
+//Function to extract information from XML
 func decodeXML(data string) (int, int, string){
-	//data := string(xmlData)
-	//fmt.Println(data)
 	buf := bytes.NewBufferString(data)
 	list := new(ListBucketResult)
 	decoded := xml.NewDecoder(buf)
@@ -84,22 +79,16 @@ func decodeXML(data string) (int, int, string){
 		fmt.Printf("Error: %v\n", err)
 	}
 
-	//fmt.Println(reflect.TypeOf(list.Content))
 	directories, extensions := getContent(list.Content)
-	//fmt.Printf("Key: %s\n", list.Content[0].Key)
-	//fmt.Printf("Title: %s\n", rss.Channel.Items[0].Description)
-	//fmt.Printf("Title: %s\n", rss.Channel.Items[1].Title)
 	return len(list.Content), directories, extensions
 }
 
 func main(){
 	if (len(os.Args) < 3){
-		//fmt.Println("No flag")
 		fmt.Println("Usage: go run aws_s3_explorer.go --bucket amazon_S3_file")
 		return
 	}
 	bucket := os.Args[1]
-	//fmt.Println(bucket)
 	if (bucket != "--bucket"){
 		fmt.Println("Incorrect flag")
 		fmt.Println("Usage: go run aws_s3_explorer.go --bucket amazon_S3_file")
@@ -107,12 +96,8 @@ func main(){
 	}
 
 	url := "https://" + os.Args[2] + ".s3.amazonaws.com"
-	//fmt.Println(url)
 	xml := getXML(url)
-	//getXML(url)
-	//fmt.Println(reflect.TypeOf(xml))
 	objects, directories, extensions := decodeXML(xml)
-	//decodeXML(xml)
 	fmt.Println("AWS S3 Explorer")
 	fmt.Printf("Bucket Name\t\t: %s\n", os.Args[2])
 	fmt.Printf("Number of objects\t: %d\n", objects)
