9916561dc57ec47f1d67d2f31b23c9aee1dda731
AcesTerra <jovengf@gmail.com>
2020-05-02 15:09:30 -0500

    REST API finished. Multiple users support
    Signed-off-by: AcesTerra <jovengf@gmail.com>

--- challenges/third-partial/rest-api.go
+++ challenges/third-partial/rest-api.go
@@ -2,36 +2,24 @@ package main
 
 import (
 	"crypto/rand"
-	//
-	//"encoding/json"
 	"fmt"
-	"image"
-	_ "image/jpeg"
-	_ "image/png"
-	"log"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 	"github.com/gin-gonic/gin"
+	"path/filepath"
+	"strconv"
 )
 
-// User struct to store users info
-type User struct {
-	Username string
-	Password string
-	Token    string
-}
-
-// Image is
-type Image struct {
-	Name  string
-	Token string
+// User struct to store users info.
+type user struct {
+	username string
+	password string
+	token    string
 }
 
 // Global variable users were users are stored
-//var users []User
-var user User
+var allUsers []user
 
 func main() {
 	r := gin.Default()
@@ -43,94 +31,105 @@ func main() {
 	authorized.GET("/login", login)
 	r.GET("/status", status)
 	r.GET("/logout", logout)
-	r.POST("/upload", addImage)
-	//router.PUT("/somePut", putting)
-	//router.DELETE("/someDelete", deleting)
-	//router.PATCH("/somePatch", patching)
-	//router.HEAD("/someHead", head)
-	//router.OPTIONS("/someOptions", options)
+	r.POST("/upload", uploadImage)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run() // Listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
-// agrega imagen y devulve el nombre y tamaño de la imagen
-func addImage(c *gin.Context) {
-	token := c.Request.Header["Authorization"]
-	t := token[0]
-	splitToken := strings.Split(t, "Bearer")
-	t = string(splitToken[1])
-	//if t==" \u003c"+user.Token+"\u003e" {
-	file, _ := c.FormFile("file")
-	log.Println(file.Filename)
-	c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
-	//	}
-
+// Upload image to server. Returns a JSON with filename and itws size.
+func uploadImage(c *gin.Context) {
+	bearer := c.Request.Header["Authorization"]
+	token := bearer[0]
+	splitedToken := strings.Split(token, " ")
+	t := string(splitedToken[1])
+	var userToken string
+	isRegistered := false
+	for _,v := range allUsers{
+		if (v.token == t){
+			isRegistered = true
+			userToken = v.token
+		}
+	}
+	if (isRegistered){
+		file, err := c.FormFile("data")
+		if err != nil {
+			c.JSON(http.StatusOK, gin.H{"message": "Error uploading image", "error": err.Error})
+			return
+		}
+		t := time.Now()
+		time := t.Format("20060102150405")
+		uploadedImage := filepath.Base(file.Filename)
+		fileName := userToken + "_" + time + "_" + filepath.Base(file.Filename)
+		if err := c.SaveUploadedFile(file, fileName); err != nil {
+			c.JSON(http.StatusOK, gin.H{"message": "Error uploading image", "error": err.Error})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{"message":"An image has been successfully uploaded", "filename": uploadedImage, "size": strconv.FormatInt(file.Size, 10) + "bytes"})
+	}
 }
 
-// hace login y muestra el token
+// Login for users. Returns a JSON with username and token generated.
 func login(c *gin.Context) {
+	var newUser user
+	userRegistered := false
 	userName, password, _ := c.Request.BasicAuth()
-	fmt.Println(userName)
-	fmt.Println(password)
-	user.Token = tokenGenerator()
-	c.JSON(http.StatusOK, gin.H{"message": "Hi username, welcome to the DPIP System", "token": user.Token})
-
+	token := tokenGenerator()
+	for i,v := range allUsers{
+		if (v.username == userName){
+			allUsers[i].token = token
+			c.JSON(http.StatusOK, gin.H{"message":"New token generated", "token": token})
+			userRegistered = true
+		}
+	}
+	if (userRegistered == false){
+		newUser.username = userName
+		newUser.password = password
+		newUser.token = token
+		allUsers = append(allUsers, newUser)
+		c.JSON(http.StatusOK, gin.H{"message":"Hi " + userName + ", welcome to the DPIP System", "token": token})
+	}
 }
 
-// borra el token
+// Revoke user token and delete user from list.
 func logout(c *gin.Context) {
-	token := c.Request.Header["Authorization"]
-	t := token[0]
-	splitToken := strings.Split(t, " ")
-	t = string(splitToken[1])
-	if t == user.Token {
-		user.Token = ""
-		c.JSON(http.StatusOK, gin.H{"message": "Bye username, your token has been revoked"})
+	bearer := c.Request.Header["Authorization"]
+	token := bearer[0]
+	splitedToken := strings.Split(token, " ")
+	t := string(splitedToken[1])
+	var userIndex int
+	var userName string
+	isRegistered := false
+	for i,v := range allUsers{
+		if (v.token == t){
+			userIndex = i
+			userName = v.username
+			isRegistered = true
+		}
+	}
+	if (isRegistered){
+		allUsers[userIndex] = allUsers[len(allUsers)-1]
+		allUsers = allUsers[:len(allUsers)-1]
+		c.JSON(http.StatusOK, gin.H{"message":"Bye " + userName + ", your token has been revoked"})
 	}
 }
 
-// devulve la hora en la que se hace la consulta
+// Status of login. Returns a JSON with username and time.
 func status(c *gin.Context) {
-	token := c.Request.Header["Authorization"]
-	t := token[0]
-	splitedToken := strings.Split(t, " ")
-	t = string(splitedToken[1])
-	if t == user.Token {
-		current := time.Now()
-		c.JSON(http.StatusOK, gin.H{"message": "Hi username, the DPIP System is Up and Running", "time": current.Format("2006-01-02 15:04:05")})
-
+	bearer := c.Request.Header["Authorization"]
+	token := bearer[0]
+	splitedToken := strings.Split(token, " ")
+	t := string(splitedToken[1])
+	for _,v := range allUsers{
+		if (v.token == t){
+			current := time.Now()
+			c.JSON(http.StatusOK, gin.H{"message":"Hi " + v.username + ", the DPIP System is Up and Running", "time": current.Format("2006-01-02 15:04:05")})
+		}
 	}
 }
 
-// genera el token
+// Generate user tokens.
 func tokenGenerator() string {
 	b := make([]byte, 8)
 	rand.Read(b)
 	return fmt.Sprintf("%x", b)
-}
-
-// devuelve las dimensiones de la imagen
-func getImageSize(imagePath string) int64 {
-	fi, err := os.Stat(imagePath)
-	if err != nil {
-		log.Println(imagePath, err)
-	}
-	// Get the size
-	size := fi.Size()
-	return size
-}
-
-// nos confunidos y primero sacamos las dimencion de la imagen
-func getImageDimension(imagePath string) (int, int) {
-	file, err := os.Open(imagePath)
-	defer file.Close()
-	if err != nil {
-		log.Println(imagePath, err)
-	}
-
-	image, _, err := image.DecodeConfig(file)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s: %v\n", imagePath, err)
-	}
-	return image.Width, image.Height
 }
